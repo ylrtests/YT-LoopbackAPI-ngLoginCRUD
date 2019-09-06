4827d6b80e8c766136ed4b84582e5a1d34bd98ce
Yojhan <yojhanr95@gmail.com>
2019-09-06 15:36:02 -0500

    Add built-in validators form Login component and Register component

--- clientBook/src/app/components/user/login/login.component.ts
+++ clientBook/src/app/components/user/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,30 +11,46 @@ import { Router } from '@angular/router'
 })
 export class LoginComponent implements OnInit {
 
+  private user: User = {
+    email: '',
+    password: ''
+  }
+  hasError: boolean = false;
+  errorMessage: string = "Verifica tus credenciales";
+
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
-  private user: User = {
-    email: '',
-    password: ''
-  }
-
   ngOnInit() {
   }
 
-  onLogin(){
-    this.authService.loginUser(this.user).subscribe(
-      (data) => {
-        this.authService.setUser(data.user);
-        this.authService.setToken(data.id);
-        this.router.navigate(['/user/profile']);
-      },
-      (err) => {
-        console.log(err)
-      }
-    )
+  onLogin(formLogin: NgForm){
+    if(formLogin.valid){
+      this.hasError = false;
+      this.authService.loginUser(this.user).subscribe(
+        (data) => {
+          this.authService.setUser(data.user);
+          this.authService.setToken(data.id);
+          this.router.navigate(['/user/profile']);
+        },
+        () => {
+          this.showError();
+        }
+      )
+    }
+    else{
+      this.showError();
+    }
+  }
+
+  showError(){
+    this.hasError = true; 
+      setTimeout( () => {
+        this.hasError = false;
+      }, 5000)
+      console.log('Error iniciando sesión');
   }
 
 }
--- clientBook/src/app/components/user/register/register.component.ts
+++ clientBook/src/app/components/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service'
 import { User } from 'src/app/models/User';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,40 +10,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
-
   private user: User = {
     name: '',
     email: '',
     password: ''
   }
 
+  hasError: boolean = false;
+  errorMessage: string = "Verifica tus credenciales";
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
   ngOnInit() {
   }
 
-  onRegister(): void {
-    this.authService.registerUser(this.user).subscribe(
-      () => {
-        this.authService.loginUser(this.user).subscribe(
-          (data) => {
-            let token = data.id;
-            this.authService.setToken(token);
-            this.authService.setUser(data.user);
-            this.router.navigate(['/user/profile']);
-          },
-          (err) => {
-            console.log("Error iniciando sesión usuario.")
-          }
-        )
-      },
-      (err) => {
-        console.log("Error registrando usuario.")
-      }
-    )
+  onRegister(formRegister: NgForm): void {
+    if (formRegister.valid) {
+      this.authService.registerUser(this.user).subscribe(
+        () => {
+          this.authService.loginUser(this.user).subscribe(
+            (data) => {
+              let token = data.id;
+              this.authService.setToken(token);
+              this.authService.setUser(data.user);
+              this.router.navigate(['/user/profile']);
+            },
+            (err) => {//Error loginUser
+              this.showError();
+              console.log(err)
+              this.errorMessage = err;
+            }
+          )
+        },
+        (err) => { //Error registerUser
+          let objectKey = Object.keys(err.error.error.details.messages)[0]
+          let message = err.error.error.details.messages[objectKey][0];
+          
+          this.errorMessage = message;
+          this.showError();
+        }
+      )
+    }
+    else{
+      this.showError();
+     }
+  }
+
+  showError(){
+    this.hasError = true; 
+      setTimeout( () => {
+        this.hasError = false;
+      }, 5000)
+      console.log('Error iniciando sesión');
   }
 
 }
